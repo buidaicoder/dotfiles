2fb89c345eb32acf0c2f2bfdfc89d6c422953798
Bui Dai <buidaicoder@gmail.com>
2023-10-20 21:00:39 +0700

    uodate

---
+++ .config/i3/script.sh
@@ -0,0 +1,8 @@
+intern=eDP1
+extern=HDMI1
+
+if xrandr | grep "$extern disconnected"; then
+    xrandr --output "$extern" --off --output "$intern" --auto
+else
+    xrandr --output "$intern" --off --output "$extern" --mode 1920x1080 --rate 75
+fi
